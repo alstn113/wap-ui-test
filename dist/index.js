abe68b267c558b211570249d8b96db61ae28110d
neko113 <alstn113@gmail.com>
2022-09-22 19:41:50 +0900

    removed cached

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-var Button = require('./components/Button/Button.js');
-
-
-
-exports.Button = Button;
--- dist/layouts/Container/Container.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-interface Props {
-    children: React.ReactNode;
-}
-declare function Container({ children }: Props): JSX.Element;
-export default Container;
